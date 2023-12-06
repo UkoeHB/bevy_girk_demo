363a3a5f5f8db3031d88a9f324a797527dfba346
koe <ukoe@protonmail.com>
2023-12-05 23:31:34 -0600

    cleanup

--- libs/bevy_girk_demo_user_client/src/game/native/local_player.rs
+++ libs/bevy_girk_demo_user_client/src/game/native/local_player.rs
@@ -23,13 +23,18 @@ const GAME_CLIENT_PATH : &'static str = concat!(env!("CARGO_MANIFEST_DIR"), "/..
 //-------------------------------------------------------------------------------------------------------------------
 //-------------------------------------------------------------------------------------------------------------------
 
-fn make_click_game_configs(game_ticks_per_sec: Ticks, game_num_ticks: Ticks) -> ClickGameFactoryConfig
+fn make_click_game_configs() -> ClickGameFactoryConfig
 {
+    // game duration
+    let game_ticks_per_sec = Ticks(20);
+    let game_num_ticks = Ticks(20 * 5);
+
     // versioning
+    //todo: use hasher directly
     let protocol_id = Rand64::new(env!("CARGO_PKG_VERSION"), 0u128).next();
 
     // config
-    let max_init_ticks  = Ticks(10);
+    let max_init_ticks  = Ticks(20 * 3);
     let game_prep_ticks = Ticks(0);
 
     // server setup config
@@ -94,9 +99,7 @@ pub(crate) fn launch_local_player_game_native(lobby_contents: ClickLobbyContents
         async move
         {
             // prep game launch pack
-            let game_ticks_per_sec = Ticks(20);
-            let game_num_ticks = Ticks(20 * 1);
-            let game_configs = make_click_game_configs(game_ticks_per_sec, game_num_ticks);
+            let game_configs = make_click_game_configs();
             let Ok(launch_pack) = get_launch_pack(ser_msg(&game_configs), lobby_contents)
             else { tracing::error!("failed getting launch pack for local player game"); return None; };
 
