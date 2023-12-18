f2773051912dfe7f0b07d423909213bc42a1e91f
koe <ukoe@protonmail.com>
2023-12-18 11:13:45 -0600

    game client: refactor to use clap for CLI args

--- bins/game_client/src/main.rs
+++ bins/game_client/src/main.rs
@@ -10,6 +10,7 @@ use bevy::prelude::*;
 use bevy_girk_game_instance::*;
 use bevy_girk_utils::*;
 use bevy_kot_utils::*;
+use clap::Parser;
 
 //standard shortcuts
 
@@ -17,23 +18,11 @@ use bevy_kot_utils::*;
 //-------------------------------------------------------------------------------------------------------------------
 //-------------------------------------------------------------------------------------------------------------------
 
-fn get_game_connect_info(mut args: std::env::Args) -> Result<GameConnectInfo, ()>
+#[derive(Parser, Debug)]
+struct GameClientCli
 {
-    // find connect info tag
-    loop
-    {
-        match args.next()
-        {
-            Some(arg) => if arg == GAME_CONNECT_INFO_TAG { break; },
-            None => return Err(()),
-        }
-    }
-
-    // extract connect info
-    let Some(arg) = args.next() else { return Err(()); };
-    let connect_info = serde_json::de::from_str::<GameConnectInfo>(&arg).map_err(|_| ())?;
-
-    Ok(connect_info)
+    #[arg(short = 'G', value_parser = parse_json::<GameConnectInfo>)]
+    connect_info: GameConnectInfo,
 }
 
 //-------------------------------------------------------------------------------------------------------------------
@@ -71,12 +60,12 @@ fn main()
 
     tracing::info!("game client started");
 
-    // get connect info as input arg
-    let connect_info = get_game_connect_info(std::env::args()).expect("game connect info is missing");
+    // CLI args
+    let args = GameClientCli::parse();
 
     // prep game core
     let protocol_id = Rand64::new(env!("CARGO_PKG_VERSION"), 0u128).next();
-    let (mut app, client_id, player_input_sender) = make_game_client_core(protocol_id, connect_info);
+    let (mut app, client_id, player_input_sender) = make_game_client_core(protocol_id, args.connect_info);
 
     // prep game skin
     prepare_game_client_skin(&mut app, client_id as u64, player_input_sender);
--- libs/bevy_girk_demo_wiring/src/game_client_process_launchers.rs
+++ libs/bevy_girk_demo_wiring/src/game_client_process_launchers.rs
@@ -9,17 +9,12 @@ use tokio::process::{Child, Command};
 
 //-------------------------------------------------------------------------------------------------------------------
 
-/// Arg tag for passing game connect info into the game client subprocess.
-pub const GAME_CONNECT_INFO_TAG: &'static str = "-gci";
-
-//-------------------------------------------------------------------------------------------------------------------
-
 pub fn launch_game_client(path: String, connect_info: &GameConnectInfo) -> Result<Child, ()>
 {
     let connect_info_ser = serde_json::to_string(&connect_info).expect("failed serializing game connect info");
 
     Command::new(path.as_str())
-        .args([GAME_CONNECT_INFO_TAG, connect_info_ser.as_str()])
+        .args(["-G", connect_info_ser.as_str()])
         .spawn()
         .map_err(|_|())
 }
