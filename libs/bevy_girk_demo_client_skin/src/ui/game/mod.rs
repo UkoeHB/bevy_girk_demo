91060a5c25c3be17a0bdc108436672798570f9c3
koe <ukoe@protonmail.com>
2023-12-19 20:06:04 -0600

    client skin: add disconnect button for testing in-app reconnects

--- libs/bevy_girk_demo_client_skin/src/ui/game/mod.rs
+++ libs/bevy_girk_demo_client_skin/src/ui/game/mod.rs
@@ -1,11 +1,13 @@
 //module tree
 mod clicker;
+mod disconnector;
 mod header;
 mod plugin;
 mod scoreboard;
 
 //API exports
 pub(crate) use crate::ui::game::clicker::*;
+pub(crate) use crate::ui::game::disconnector::*;
 pub(crate) use crate::ui::game::header::*;
 pub(crate) use crate::ui::game::plugin::*;
 pub(crate) use crate::ui::game::scoreboard::*;
--- libs/bevy_girk_demo_client_skin/src/ui/game/plugin.rs
+++ libs/bevy_girk_demo_client_skin/src/ui/game/plugin.rs
@@ -16,6 +16,7 @@ pub(crate) fn add_game(ui: &mut UiBuilder<MainUi>, area: &Widget)
     ui.div_rel(area.end(""), (0., 100.), ( 0.,  10.), add_game_header);
     ui.div_rel(area.end(""), (0., 100.), (10.,  65.), add_game_scoreboard);
     ui.div_rel(area.end(""), (0., 100.), (65., 100.), add_game_clicker);
+    ui.div_rel(area.end(""), (3.,  13.), (90.,  95.), add_disconnector);
 }
 
 //-------------------------------------------------------------------------------------------------------------------
@@ -25,7 +26,8 @@ pub(crate) fn UiGamePlugin(app: &mut App)
 {
     app.add_plugins(UiGameHeaderPlugin)
         .add_plugins(UiGameScoreboardPlugin)
-        .add_plugins(UiGameClickerPlugin);
+        .add_plugins(UiGameClickerPlugin)
+        .add_plugins(UiGameDisconnectorPlugin);
 }
 
 //-------------------------------------------------------------------------------------------------------------------
