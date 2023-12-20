c6a0c72f48d9f17fb1593be963e8cdf384843e19
koe <ukoe@protonmail.com>
2023-12-20 12:28:22 -0600

    wiring: update game factory to use new bevy_girk utilities

--- libs/bevy_girk_demo_wiring/src/game_factory.rs
+++ libs/bevy_girk_demo_wiring/src/game_factory.rs
@@ -5,8 +5,6 @@ use bevy_girk_demo_game_core::*;
 
 //third-party shortcuts
 use bevy::prelude::*;
-#[allow(unused_imports)]
-use bevy_renet::renet::transport::{generate_random_bytes, ServerAuthentication, ServerConfig};
 use bevy_girk_client_fw::*;
 use bevy_girk_game_fw::*;
 use bevy_girk_game_instance::*;
@@ -16,9 +14,19 @@ use serde::{Deserialize, Serialize};
 
 //standard shortcuts
 use std::collections::{HashMap, HashSet};
-use std::net::SocketAddr;
 use std::vec::Vec;
-use wasm_timer::{SystemTime, UNIX_EPOCH};
+
+//-------------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------------
+
+struct GameStartupHelper
+{
+    fw_init      : GameFWInitializer,
+    click_init   : ClickGameInitializer,
+    clients      : Vec<(u128, ClientIdType)>,
+    native_count : usize,
+    wasm_count   : usize,
+}
 
 //-------------------------------------------------------------------------------------------------------------------
 //-------------------------------------------------------------------------------------------------------------------
@@ -27,17 +35,19 @@ use wasm_timer::{SystemTime, UNIX_EPOCH};
 fn prepare_game_startup(
     client_init_data     : &Vec<ClientInitDataForGame>,
     game_duration_config : GameDurationConfig
-) -> Result<(GameFWInitializer, ClickGameInitializer, Vec<(bevy_simplenet::EnvType, u128, ClientIdType)>), ()>
+) -> Result<GameStartupHelper, ()>
 {
     // prepare each client
     let mut client_states = Vec::<ClientState>::new();
     let mut players       = HashMap::<ClientIdType, PlayerState>::new();
     let mut watchers      = HashSet::<ClientIdType>::new();
-    let mut user_clients  = Vec::<(bevy_simplenet::EnvType, u128, ClientIdType)>::new();
+    let mut clients       = Vec::<(u128, ClientIdType)>::new();
+    let mut native_count  = 0;
+    let mut wasm_count    = 0;
     client_states.reserve(client_init_data.len());
     players.reserve(client_init_data.len());
     watchers.reserve(client_init_data.len());
-    user_clients.reserve(client_init_data.len());
+    clients.reserve(client_init_data.len());
 
     for client_init in client_init_data.iter()
     {
@@ -77,29 +87,27 @@ fn prepare_game_startup(
                     }
             );
 
+        // count client type
+        match client_init.env
+        {
+            bevy_simplenet::EnvType::Native => native_count += 1,
+            bevy_simplenet::EnvType::Wasm => wasm_count += 1,
+        }
+
         // save user_id/client_id mapping
-        user_clients.push((client_init.env, client_init.user_id, client_id));
+        clients.push((client_init.user_id, client_id));
     }
 
     // finalize
-    let seed =
-        {
-            #[cfg(target_family = "wasm")]
-            {
-                // we assume game instances are only created on WASM for local single-player
-                SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis()
-            }
-            #[cfg(not(target_family = "wasm"))]
-            {
-                gen_rand128()
-            }
-        };
+    let game_context = ClickGameContext::new(gen_rand128(), game_duration_config);
 
-    let game_fw_initializer = GameFWInitializer{ clients: client_states };
-    let game_context        = ClickGameContext::new(seed, game_duration_config);
-    let game_initializer    = ClickGameInitializer{ game_context, players, watchers };
-
-    Ok((game_fw_initializer, game_initializer, user_clients))
+    Ok(GameStartupHelper{
+        fw_init    : GameFWInitializer{ clients: client_states },
+        click_init : ClickGameInitializer{ game_context, players, watchers },
+        clients,
+        native_count,
+        wasm_count,
+    })
 }
 
 //-------------------------------------------------------------------------------------------------------------------
@@ -148,48 +156,22 @@ fn prepare_client_start_pack(
 //-------------------------------------------------------------------------------------------------------------------
 //-------------------------------------------------------------------------------------------------------------------
 
-fn new_server_config(num_clients: usize, server_setup_config: &GameServerSetupConfig, auth_key: &[u8; 32]) -> ServerConfig
-{
-    ServerConfig{
-            current_time     : SystemTime::now().duration_since(UNIX_EPOCH).unwrap(),
-            max_clients      : num_clients,
-            protocol_id      : server_setup_config.protocol_id,
-            public_addresses : vec![SocketAddr::new(server_setup_config.server_ip.into(), 0)],
-            authentication   : ServerAuthentication::Secure{ private_key: *auth_key },
-        }
-}
-
-//-------------------------------------------------------------------------------------------------------------------
-//-------------------------------------------------------------------------------------------------------------------
-
 /// Prepare game start report to assist participants with setting up their game clients.
-fn prepare_game_start_report(
-    app              : &App,
-    server_config    : &GameServerSetupConfig,
-    auth_key         : &[u8; 32],
-    user_clients     : &Vec<(bevy_simplenet::EnvType, u128, ClientIdType)>,
-    server_addresses : Vec<SocketAddr>,
-) -> Result<GameStartReport, ()>
+fn get_game_start_infos(
+    app          : &App,
+    user_clients : &Vec<(u128, ClientIdType)>,
+) -> Result<Vec<GameStartInfo>, ()>
 {
     // extract data
     let game_initializer = app.world.resource::<ClickGameInitializer>();
     let ticks_per_sec    = app.world.resource::<GameFWConfig>().ticks_per_sec();
 
     // make start infos for each client
-    let mut native_count = 0;
-    let mut wasm_count = 0;
     let mut start_infos = Vec::<GameStartInfo>::new();
     start_infos.reserve(user_clients.len());
 
-    for (env, user_id, client_id) in user_clients.iter()
+    for (user_id, client_id) in user_clients.iter()
     {
-        // count client types
-        match env
-        {
-            bevy_simplenet::EnvType::Native => native_count += 1,
-            bevy_simplenet::EnvType::Wasm   => wasm_count += 1,
-        }
-
         // get game start package for client
         let client_start_pack = prepare_client_start_pack(&*game_initializer, *client_id, ticks_per_sec)?;
 
@@ -203,29 +185,7 @@ fn prepare_game_start_report(
             );
     }
 
-    // prepare connect metas based on client types
-    let mut native_meta = None;
-    let wasm_meta = None;
-
-    if native_count > 0
-    {
-        native_meta = Some(GameServerConnectMetaNative{
-            server_config    : server_config.clone(),
-            server_addresses : server_addresses.clone(),
-            auth_key         : auth_key.clone(),
-        });
-    }
-
-    if native_count == 0 && wasm_count == 1
-    {
-        tracing::error!("wasm single-player clients not yet supported");
-    }
-    else if wasm_count > 1
-    {
-        tracing::error!("wasm multi-player clients not yet supported");
-    }
-
-    Ok(GameStartReport{ native_meta, wasm_meta, start_infos })
+    Ok(start_infos)
 }
 
 //-------------------------------------------------------------------------------------------------------------------
@@ -284,47 +244,24 @@ impl GameFactoryImpl for ClickGameFactory
         else { tracing::error!("could not deserialize click game factory config"); return Err(()); };
 
         // initialize clients and game config
-        let (game_fw_initializer, game_initializer, user_clients) = prepare_game_startup(
-                &launch_pack.client_init_data,
-                config.game_duration_config
-            )?;
+        let startup = prepare_game_startup(&launch_pack.client_init_data, config.game_duration_config)?;
 
         // prepare game app
-        prepare_game_app_framework(app, config.game_fw_config, game_fw_initializer);
-        prepare_game_app_replication(app);
-        prepare_game_app_core(app, game_initializer);
+        let (native_meta, wasm_meta) = prepare_game_app_backend(
+            app,
+            config.game_fw_config,
+            startup.fw_init,
+            config.server_setup_config,
+            startup.native_count,
+            startup.wasm_count
+        );
+        prepare_game_app_core(app, startup.click_init);
 
-        // set up renet server
-        // - we use a unique auth key so clients can only interact with the server created here
-        //todo: wasm single player, we don't need auth key, just use in-memory transport (need server config enum)
-        //todo: set up renet server transports based on client types
-        let auth_key =
-            {
-                #[cfg(target_family = "wasm")]
-                {
-                    if user_clients.len() != 1 { panic!("only single-player game instances may be created on WASM targets"); }
-                    Default::default()
-                }
-                #[cfg(not(target_family = "wasm"))]
-                {
-                    generate_random_bytes::<32>()
-                }
-            };
-
-        let server_config = new_server_config(launch_pack.client_init_data.len(), &config.server_setup_config, &auth_key);
-        let server_addr = setup_native_renet_server(app, server_config);
-
-        // prepare game start report
+        // game start info
         // - must call this AFTER prepping the game app and setting up the renet server
-        let game_start_report = prepare_game_start_report(
-                &app,
-                &config.server_setup_config,
-                &auth_key,
-                &user_clients,
-                vec![server_addr]
-            )?;
+        let start_infos = get_game_start_infos(&app, &startup.clients)?;
 
-        Ok(game_start_report)
+        Ok(GameStartReport{ native_meta, wasm_meta, start_infos })
     }
 }
 
